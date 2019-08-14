0bc69404e90560db6a22348c6e6d0a1ee25109ee
David Acosta <davidacosta25081@yahoo.com>
2019-08-14 06:11:10 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,7 +11,7 @@ class AuthorsController < ApplicationController
     @author = Author.new(author_params)
     if @author.save
       redirect_to author_path(@author)
-    else 
+    else
       render :new
     end
   end
